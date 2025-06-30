4f5cbd3fa53b9bdb476d4c78043a737f306b0661
devdatta95 <devdattasupnekar.ds@gmail.com>
2025-06-30 11:38:25 +0530

    updated main file and readme file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 git remote add origin https://github.com/devdatta95/test-project.git
+git add .
+git commit -m "first commit" 
 git branch -M main
 git push -u origin main
--- main.py
+++ main.py
@@ -2,5 +2,15 @@ def main():
     print("Hello from test-project!")
 
 
+def add(x, y):
+    return x+y
+
+print(add(3,4))
+
+print("devdatta")
+
+
 if __name__ == "__main__":
+    print()
     main()
+    print()
